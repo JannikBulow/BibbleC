6240e2929b3ef01f3503d409879d7eedc4a1452d
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-07-02 00:36:48 +0200

    feat(scope): add scope skeleton

---
+++ framework/include/BibbleC/scope/scope.h
@@ -0,0 +1,15 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef BIBBLEC_SCOPE_SCOPE_H
+#define BIBBLEC_SCOPE_SCOPE_H
+
+#include "BibbleC/api.h"
+
+namespace bibblec::scope {
+    class BIBBLEC_EXPORT Scope {
+    public:
+
+    };
+}
+
+#endif //BIBBLEC_SCOPE_SCOPE_H
