e9abdcf54985424913037afb68b21aaa226bfd3a
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-07-01 18:07:03 +0200

    chore: initial project setup

---
+++ framework/include/BibbleC/diagnostic/diagnostic.h
@@ -0,0 +1,10 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef BIBBLEC_DIAGNOSTIC_DIAGNOSTIC_H
+#define BIBBLEC_DIAGNOSTIC_DIAGNOSTIC_H
+
+namespace bibblec::diagnostic {
+
+}
+
+#endif //BIBBLEC_DIAGNOSTIC_DIAGNOSTIC_H
---
+++ framework/src/diagnostic/diagnostic.cpp
@@ -0,0 +1,3 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#include "BibbleC/diagnostic/diagnostic.h"
