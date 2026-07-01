ad4d18266bf0b844ca3ab2a0563a71ff3c6eff1b
Jannik Laugmand Bülow <jannikbulow0@gmail.com>
2026-07-02 00:33:53 +0200

    feat(debug): add source pair

---
+++ framework/include/BibbleC/debug/source_pair.h
@@ -0,0 +1,15 @@
+// Copyright 2026 Jannik Laugmand Bülow
+
+#ifndef BIBBLEC_DEBUG_SOURCE_PAIR_H
+#define BIBBLEC_DEBUG_SOURCE_PAIR_H
+
+#include "BibbleC/lexer/SourceLocation.h"
+
+namespace bibblec {
+    struct SourcePair {
+        lexer::SourceLocation start;
+        lexer::SourceLocation end;
+    };
+}
+
+#endif //BIBBLEC_DEBUG_SOURCE_PAIR_H
